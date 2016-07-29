b4a9e67035a102fcc15499129ef139094cf57e93
skylarhansen <skylarshansen@gmail.com>
2016-07-29 11:45:36 -0600

    final first version

--- BornReady/AppDelegate.swift
+++ BornReady/AppDelegate.swift
@@ -15,7 +15,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
-        NSThread.sleepForTimeInterval(0.5)
+        NSThread.sleepForTimeInterval(0.75)
         return true
     }
 
